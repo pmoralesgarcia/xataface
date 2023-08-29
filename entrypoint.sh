e3642bd5fb5fa3f7686b5d56cfc4d738b8a6117d
Manuel Nila <3621543+manila@users.noreply.github.com>
2023-08-28 23:51:00 -0400

    entrypoint.sh: Start php

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-php makesite "$SITE_PATH" $DB_USER:$DB_PASS@$DB_HOST/$DB_NAME "$SITE_URL"
+php makesite $SITE_PATH $DB_USER:$DB_PASS@$DB_HOST/$DB_NAME "$SITE_URL"
+php-fpm8.2
+nginx -g 'daemon off;'
